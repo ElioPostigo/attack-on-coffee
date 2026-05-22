a0d5adab02e8bdc9e741830c2a64274063728097
Elio M. <eliomiguelpostigobenitez@gmail.com>
2026-05-22 17:40:21 +0200

    casi completada

--- core/src/main/java/com/politecnicomalaga/attack_on_coffee/model/filaClientes.java
+++ core/src/main/java/com/politecnicomalaga/attack_on_coffee/model/filaClientes.java
@@ -1,14 +1,16 @@
 package com.politecnicomalaga.attack_on_coffee.model;
-
 import java.util.ArrayList;
 
-public class FilaClientes {
+public class filaClientes {
 
     private ArrayList<Cliente> clientes;
     private int velocidadHorizontal;
     private boolean moviendoDerecha;
 
     public FilaClientes(int velocidadHorizontal) {
+        this.clientes = new ArrayList<>();
+        this.velocidadHorizontal = velocidadHorizontal;
+        this.moviendoDerecha = true;
     }
 
     public void agregarCliente(Cliente cliente) {
@@ -16,6 +18,15 @@ public class FilaClientes {
     }
 
     public void moverHorizontal() {
+
+        for (Cliente cliente : clientes) {
+
+            if (moviendoDerecha) {
+                cliente.setX(cliente.getX() + velocidadHorizontal);
+            } else {
+                cliente.setX(cliente.getX() - velocidadHorizontal);
+            }
+        }
     }
 
     public void cambiarDireccion() {
