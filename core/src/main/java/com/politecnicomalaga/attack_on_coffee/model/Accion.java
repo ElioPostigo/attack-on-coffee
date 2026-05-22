5301b8e6b5a64abd43f3325e21f771d335233c72
Mariano <marianodelaperez72@gmail.com.com>
2026-05-22 19:50:51 +0200

    Subiendo la clase accion completa (Revisen mi mmd por favor)

---
+++ core/src/main/java/com/politecnicomalaga/attack_on_coffee/model/Accion.java
@@ -0,0 +1,29 @@
+package com.politecnicomalaga.attack_on_coffee.model;
+
+import com.badlogic.gdx.graphics.Texture;
+public abstract class Accion {
+    private float velocidad;
+    //Para Controlar si ha impactado el proyectil/accion/bala/ataque y etc
+    private boolean impactado;
+    public Accion(Texture img, float x, float y,float velocidad){
+        super(img,x,y);
+        this.velocidad=velocidad;
+        this.impactado = false;
+    }
+    //para definir como se va a mover cada accion
+    public abstract void mover();
+    public float getVelocidad(){
+        return velocidad;
+    }
+    public void setVelocidad(){
+        this.velocidad=velocidad;
+    }
+    public boolean getImpactado(){
+        return impactado;
+    }
+    //Si al final la proyectil/accion/bala/ataque impacta al objetivo se desactiva
+    public void setImpactado(){
+        this.impactado=impactado;
+        if(impactado)setActivo(false);
+    }
+}
