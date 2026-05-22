dbaf88f05a1e856b0517aabdd768d86107ca1547
Elio M. <eliomiguelpostigobenitez@gmail.com>
2026-05-22 11:59:54 +0200

    Segunda version cliente, menos contenido, puede faltar algo

--- core/src/main/java/com/politecnicomalaga/attack_on_coffee/model/Cliente.java
+++ core/src/main/java/com/politecnicomalaga/attack_on_coffee/model/Cliente.java
@@ -1,6 +1,7 @@
 package com.politecnicomalaga.attack_on_coffee.model;
 
 public class Cliente extends Persona {
+
     public Cliente(int x, int y, int vida, int velocidad) {
         super(x, y, vida, velocidad);
     }
@@ -10,20 +11,7 @@ public class Cliente extends Persona {
         x += velocidad;
     }
 
-    @Override
-    public void recibirDanio(int cantidad) {
-        vida -= cantidad;
-
-        if (vida <= 0) {
-            morir();
-        }
-    }
-
     public Queja lanzarQueja() {
         return new Queja(x, y);
-    }
-
-    public void morir() {
-        //ns que va aqui;
     }
 }
