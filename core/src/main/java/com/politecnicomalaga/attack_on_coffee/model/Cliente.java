3bd1e2befd94de03aaf2c77456eea5bab8c31f1a
Elio M. <eliomiguelpostigobenitez@gmail.com>
2026-05-22 17:46:51 +0200

    Arreglar errores y añadir lo relacionado con quejas

--- core/src/main/java/com/politecnicomalaga/attack_on_coffee/model/Cliente.java
+++ core/src/main/java/com/politecnicomalaga/attack_on_coffee/model/Cliente.java
@@ -1,17 +1,37 @@
 package com.politecnicomalaga.attack_on_coffee.model;
 
+import java.util.ArrayList;
+
 public class Cliente extends Persona {
+
+    private ArrayList<Queja> quejas;
 
     public Cliente(int x, int y, int vida, int velocidad) {
         super(x, y, vida, velocidad);
+        this.quejas = new ArrayList<>();
     }
 
     @Override
     public void mover() {
-        x += velocidad;
+        setX(getX() + velocidad);
     }
 
-    public Queja lanzarQueja() {
-        return new Queja(x, y);
+    public void crearQueja() {
+        Queja nuevaQueja = new Queja(getX(), getY());
+        quejas.add(nuevaQueja);
+    }
+
+    public ArrayList<Queja> getQuejas() {
+        return quejas;
+    }
+
+    public void moverQuejas() {
+        for (Queja queja : quejas) {
+            queja.mover();
+        }
+    }
+
+    public void eliminarQueja(Queja queja) {
+        quejas.remove(queja);
     }
 }
