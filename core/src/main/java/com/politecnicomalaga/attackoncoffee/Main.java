e4dfe869d53fd2da1f25b6abd66eefe6cfe2830f
HotGuy <javibot00@gmail.com>
2026-05-24 18:49:42 +0200

    [juego] Limpieza de código e imports en Main.java y Main.gwt.xml

--- core/src/main/java/com/politecnicomalaga/attackoncoffee/Main.java
+++ core/src/main/java/com/politecnicomalaga/attackoncoffee/Main.java
@@ -1,13 +1,11 @@
 package com.politecnicomalaga.attackoncoffee;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 import com.politecnicomalaga.attackoncoffee.screen.MenuScreen;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+/**
+ * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms.
+ */
 public class Main extends Game {
 
     @Override
