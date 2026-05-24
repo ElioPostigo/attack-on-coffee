ff493d3009f9d54caf5503fdf4bfc4da3578042a
Elio M. <eliomiguelpostigobenitez@gmail.com>
2026-05-24 17:48:36 +0200

    Cliente cambios realizados

--- core/src/main/java/com/politecnicomalaga/attackoncoffee/model/Cliente.java
+++ core/src/main/java/com/politecnicomalaga/attackoncoffee/model/Cliente.java
@@ -13,7 +13,6 @@ public class Cliente extends Entidad {
     }
 
     public void lanzarQueja(Texture texturaQueja) {
-
         Queja q = new Queja(
             texturaQueja,
             getSprite().getX(),
