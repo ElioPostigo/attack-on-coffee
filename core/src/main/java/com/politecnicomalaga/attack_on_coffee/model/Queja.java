3431e83c0bbc9ed576f124d367502cf632affd94
Mariano De la Perez <marianodelaperez72@gmail.com>
2026-05-25 12:01:57 +0200

    Subiendo la clase 'Queja' y cambiando levemente la funcion mover

---
+++ core/src/main/java/com/politecnicomalaga/attack_on_coffee/model/Queja.java
@@ -0,0 +1,23 @@
+package com.politecnicomalaga.attackoncoffee.model;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Cafe extends Accion {
+
+    //La velocidad se puede modificar, podemos hacer fija cambiando la variable velocidad por un numero
+    //O dejarla asi para cuando queramos subirle la dificultad dependiendo el nivel
+    public Cafe(Texture img, float x, float y, float velocidad) {
+        super(img, x, y, velocidad);
+    }
+
+    //Es el movimiento pero al reves
+    @Override
+    public void mover() {
+        getSprite().translateY(-getVelocidad() * Gdx.graphics.getDeltaTime());
+        updatePosition(0);
+
+        if (getSprite().getY() < 0) {
+            setActivo(false);
+        }
+    }
+}
