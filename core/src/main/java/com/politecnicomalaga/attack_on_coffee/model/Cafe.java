987f419397edce1990ae92965124e49be04d32da
marianodelaperez72 <marianodelaperez72@gmail.com>
2026-05-26 10:05:39 +0200

    Arreglando fallos tontos

--- core/src/main/java/com/politecnicomalaga/attack_on_coffee/model/Cafe.java
+++ core/src/main/java/com/politecnicomalaga/attack_on_coffee/model/Cafe.java
@@ -1,18 +1,19 @@
 package com.politecnicomalaga.attackoncoffe.model;
 import com.badlogic.gdx.graphics.Texture;
-
-public class Cafe(Texture img, float x, float y, float velocidad){
-    super(img,x,y,velocidad); 
-}
-
-//Esto es para que la bala amiga suba hacia arriba
-@Override
-public void mover(){
-    getSprite().translateY(getVelocidad() * Gdx.graphics.getDeltaTime()); // ✅ sube
-    updatePosition(0);
-
-    //Ejemplo de que pasa si sale de la pantalla
-    if(getSprite().getY()>720){
-        setActivo(false);
+public class Cafe extends Accion {
+    public class Cafe(Texture img, float x, float y, float velocidad){
+        super(img,x,y,velocidad); 
+    }
+    
+    //Esto es para que la bala amiga suba hacia arriba
+    @Override
+    public void mover(){
+        getSprite().translateY(getVelocidad() * Gdx.graphics.getDeltaTime()); 
+        updatePosition(0);
+    
+        //Ejemplo de que pasa si sale de la pantalla
+        if(getSprite().getY()>720){
+            setActivo(false);
+        }
     }
 }
