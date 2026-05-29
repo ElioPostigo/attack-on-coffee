a870f6c5349ccdc876a8ec0e332ba0f4687491ae
Elio M. <eliomiguelpostigobenitez@gmail.com>
2026-05-29 12:56:19 +0200

    Cambio de codigo tras problemas en la ejecucion

--- core/src/main/java/com/politecnicomalaga/attackoncoffee/model/FilaClientes.java
+++ core/src/main/java/com/politecnicomalaga/attackoncoffee/model/FilaClientes.java
@@ -1,24 +1,20 @@
 package com.politecnicomalaga.attackoncoffee.model;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 
 public class FilaClientes {
 
     private Cliente[] clientes;
-    private float velocidad;
 
     public FilaClientes(
         int cantidad,
         Texture textura,
         float xInicial,
         float y,
-        float separacion,
-        float velocidad
+        float separacion
     ) {
 
-        this.velocidad = velocidad;
-        this.clientes = new Cliente[cantidad];
+        clientes = new Cliente[cantidad];
 
         for (int i = 0; i < cantidad; i++) {
 
@@ -28,11 +24,7 @@ public class FilaClientes {
         }
     }
 
-    // Devuelve true si toca un borde
-    // Mueve la fila, detecta bordes, y cambia la direccion
-    public boolean moverHorizontal(float delta) {
-
-        boolean cambiarDireccion = false;
+    public void moverHorizontal(float velocidad, float delta) {
 
         for (Cliente c : clientes) {
 
@@ -41,28 +33,8 @@ public class FilaClientes {
                 c.getSprite().translateX(velocidad * delta);
 
                 c.updatePosition(delta);
-
-                // Borde derecho
-                if (c.getSprite().getX() + c.getSprite().getWidth()
-                    >= Gdx.graphics.getWidth()) {
-
-                    cambiarDireccion = true;
-                }
-
-                // Borde izquierdo
-                if (c.getSprite().getX() <= 0) {
-
-                    cambiarDireccion = true;
-                }
             }
         }
-
-        // Cambiar dirección
-        if (cambiarDireccion) {
-            velocidad = -velocidad;
-        }
-
-        return cambiarDireccion;
     }
 
     public Cliente[] getClientes() {
