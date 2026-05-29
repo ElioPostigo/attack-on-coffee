7ffe0cca394669178b1b66267c6ccf3b1d87d410
HotGuy <javibot00@gmail.com>
2026-05-29 19:34:32 +0200

    [barista] Refactorización de GrupoClientes, Cliente y FilaClientes y limpieza de código

--- core/src/main/java/com/politecnicomalaga/attackoncoffee/model/FilaClientes.java
+++ core/src/main/java/com/politecnicomalaga/attackoncoffee/model/FilaClientes.java
@@ -4,34 +4,21 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class FilaClientes {
 
-    private Cliente[] clientes;
+    private final Cliente[] clientes;
 
-    public FilaClientes(
-        int cantidad,
-        Texture textura,
-        float xInicial,
-        float y,
-        float separacion
-    ) {
-
+    public FilaClientes(int cantidad, Texture textura, float xInicial, float y, float separacion) {
         clientes = new Cliente[cantidad];
 
         for (int i = 0; i < cantidad; i++) {
-
             float x = xInicial + (i * separacion);
-
             clientes[i] = new Cliente(textura, x, y);
         }
     }
 
     public void moverHorizontal(float velocidad, float delta) {
-
         for (Cliente c : clientes) {
-
             if (c != null && c.isActivo()) {
-
                 c.getSprite().translateX(velocidad * delta);
-
                 c.updatePosition(delta);
             }
         }
--- core/src/main/java/com/politecnicomalaga/attackoncoffee/model/GrupoClientes.java
+++ core/src/main/java/com/politecnicomalaga/attackoncoffee/model/GrupoClientes.java
@@ -5,64 +5,38 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class GrupoClientes {
 
-    private FilaClientes[] filas;
-
+    private final FilaClientes[] filas;
+    private final float velocidadVertical;
     private float velocidadHorizontal;
-    private float velocidadVertical;
 
-    public GrupoClientes(
-        int numFilas,
-        int clientesPorFila,
-        Texture textura,
-        float xInicial,
-        float yInicial,
-        float separacionHorizontal,
-        float separacionVertical,
-        float velocidadHorizontal,
-        float velocidadVertical
-    ) {
-
+    public GrupoClientes(int numFilas, int clientesPorFila, Texture textura, float xInicial, float yInicial,
+                         float separacionHorizontal, float separacionVertical, float velocidadHorizontal, float velocidadVertical) {
         this.velocidadHorizontal = velocidadHorizontal;
         this.velocidadVertical = velocidadVertical;
 
         filas = new FilaClientes[numFilas];
 
         for (int i = 0; i < numFilas; i++) {
-
             float y = yInicial - (i * separacionVertical);
-
-            filas[i] = new FilaClientes(
-                clientesPorFila,
-                textura,
-                xInicial,
-                y,
-                separacionHorizontal
-            );
+            filas[i] = new FilaClientes(clientesPorFila, textura, xInicial, y, separacionHorizontal);
         }
     }
 
     public void mover(float delta) {
-
         boolean tocarBorde = false;
 
         // Detectar bordes
         for (FilaClientes fila : filas) {
-
             for (Cliente c : fila.getClientes()) {
-
                 if (c != null && c.isActivo()) {
 
                     // Derecha
-                    if (c.getSprite().getX()
-                        + c.getSprite().getWidth()
-                        >= Gdx.graphics.getWidth()) {
-
+                    if (c.getSprite().getX() + c.getSprite().getWidth() >= Gdx.graphics.getWidth()) {
                         tocarBorde = true;
                     }
 
                     // Izquierda
                     if (c.getSprite().getX() <= 0) {
-
                         tocarBorde = true;
                     }
                 }
@@ -71,17 +45,11 @@ public class GrupoClientes {
 
         // Si toca borde:
         if (tocarBorde) {
-
             velocidadHorizontal = -velocidadHorizontal;
-
             for (FilaClientes fila : filas) {
-
                 for (Cliente c : fila.getClientes()) {
-
                     if (c != null && c.isActivo()) {
-
                         c.getSprite().translateY(-velocidadVertical);
-
                         c.updatePosition(delta);
                     }
                 }
@@ -90,7 +58,6 @@ public class GrupoClientes {
 
         // Movimiento horizontal
         for (FilaClientes fila : filas) {
-
             fila.moverHorizontal(velocidadHorizontal, delta);
         }
     }
