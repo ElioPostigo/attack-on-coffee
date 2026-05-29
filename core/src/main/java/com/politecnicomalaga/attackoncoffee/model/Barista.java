ef8a38f4b3b14f31ce1391f50023c9a0837df2cf
HotGuy <javibot00@gmail.com>
2026-05-29 23:59:41 +0200

    [try-android] Implement shooting mechanics, add sounds, and refactor model class formatting

--- core/src/main/java/com/politecnicomalaga/attackoncoffee/model/Barista.java
+++ core/src/main/java/com/politecnicomalaga/attackoncoffee/model/Barista.java
@@ -2,22 +2,42 @@ package com.politecnicomalaga.attackoncoffee.model;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Barista extends Persona {
+import java.util.ArrayList;
+import java.util.List;
 
-    public Barista(Texture img, float x, float y, float velocidadX) {
-        super(img, x, y, velocidadX, 0f);
+public class Barista extends Entidad {
+
+    private final List<Cafe> cafesEnPantalla;
+    private float tiempoDisparo; // Cooldown entre disparos
+    private final Sprite sprite;
+    private float velocidadX;
+    private final Texture cafeTexture ;
+    private final Sound shootSound;
+
+    public Barista(Texture img, float x, float y) {
+        super(img, x, y);
+        this.velocidadX = 0f;
+        this.cafesEnPantalla = new ArrayList<>();
+        this.tiempoDisparo = 1f;
+        sprite = getSprite();
+        velocidadX = 200f;
+        cafeTexture= new Texture("cafe.png");
+        shootSound = Gdx.audio.newSound(Gdx.files.internal("sounds/shootSound.mp3"));
     }
 
-    @Override
-    public void mover(float delta) {
+    public void updatePosition(float delta) {
+        super.updatePosition();
         // --- CONTROL PARA PC (TECLADO) ---
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A)) {
-            getSprite().translateX(-getVelocidadX() * delta);
+            getSprite().translateX(-this.velocidadX * delta);
         }
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D)) {
-            getSprite().translateX(getVelocidadX() * delta);
+            getSprite().translateX(this.velocidadX * delta);
         }
 
         // --- CONTROL PARA ANDROID (PANTALLA TÁCTIL) ---
@@ -29,11 +49,11 @@ public class Barista extends Persona {
 
             // Si toca en la mitad izquierda, se desplaza a la izquierda
             if (toqueX < mitadPantalla) {
-                getSprite().translateX(-getVelocidadX() * delta);
+                getSprite().translateX(-this.velocidadX * delta);
             }
             // Si toca en la mitad derecha, se desplaza a la derecha
             else {
-                getSprite().translateX(getVelocidadX() * delta);
+                getSprite().translateX(this.velocidadX * delta);
             }
         }
 
@@ -45,13 +65,54 @@ public class Barista extends Persona {
             getSprite().setX(Gdx.graphics.getWidth() - getSprite().getWidth());
         }
 
-        updatePosition();
+        // Disparo
+        tiempoDisparo -= delta;
+        if (tiempoDisparo <= 0) {
+            disparar();
+            tiempoDisparo = 1f;
+        }
+
+        // Actualizar disparos
+        for (int i = cafesEnPantalla.size() - 1; i >= 0; i--) {
+            Cafe cafe = cafesEnPantalla.get(i);
+            cafe.mover();
+            cafe.updatePosition();
+            if (!cafe.isActivo()) {
+                cafesEnPantalla.remove(i);
+            }
+        }
     }
 
-    public Cafe disparar(Texture texturaCafe, float velocidadVerticalCafe) {
-        float xDisparo = getSprite().getX() + (getSprite().getWidth() / 2f) - (texturaCafe.getWidth() / 2f);
-        float yDisparo = getSprite().getY() + getSprite().getHeight();
-
-        return new Cafe(texturaCafe, xDisparo, yDisparo, velocidadVerticalCafe);
+    public void disparar() {
+        // Crear un nuevo disparo (café) en la posición del barista
+        Texture cafeTexture = new Texture("cafe.png"); // Cargar textura
+        Cafe cafe = new Cafe(cafeTexture, sprite.getX(), sprite.getY(), 200f);
+        shootSound.play(1.2f);
+        cafesEnPantalla.add(cafe);
     }
+
+//    public Cafe disparar(Texture texturaCafe, float velocidadVerticalCafe) {
+//        float xDisparo = getSprite().getX() + (getSprite().getWidth() / 2f) - (texturaCafe.getWidth() / 2f);
+//        float yDisparo = getSprite().getY() + getSprite().getHeight();
+//
+//        return new Cafe(texturaCafe, xDisparo, yDisparo, velocidadVerticalCafe);
+//    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        super.draw(batch);
+        for (Cafe cafe : cafesEnPantalla) {
+            cafe.draw(batch);
+        }
+    }
+
+    public List<Cafe> getCafes() {
+        return cafesEnPantalla;
+    }
+
+    public void dispose(){
+        if (shootSound != null) shootSound.dispose();
+        cafeTexture.dispose();
+    }
+
 }
